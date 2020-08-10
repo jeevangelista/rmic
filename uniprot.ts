fec934609d7ae17784dd98f5ac4ad5d68beae700
erol <je.evangelista04@gmail.com>
2020-08-10 18:00:53 -0400

    Edited uniprot.ts

--- uniprot.ts
+++ uniprot.ts
@@ -1,28 +1,26 @@
-import axios from 'axios'
+import fetch from 'node-fetch'
 import {strict as assert} from 'assert' // node env v 9 did not support "import assert..."
 
-function mapToUniPort(prot:string): Promise<any> {
-  return axios.get("https://www.ebi.ac.uk/proteins/api/proteins", {
-      params: {
-        offset: 0,
-        protein: prot
-      }
-    })
-    .then(res => 
-    {let proteins = res.data.filter(
-      function(x:object) {return ("protein" in x)})
-    //console.log("Number of protein entries found: " + proteins.length)
-    return proteins
-    })
-    .catch(err => {throw(err)})
+export async function mapToUniPort(prot: string): Promise<any> {
+  const url = `https://www.ebi.ac.uk/proteins/api/proteins?offset=0&protein=${prot}`
+  const results = await (await fetch(url, 
+    {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    }
+  )).json()
+  return results.filter(r=>r.protein!==undefined)
 }
   
   
-export default async function(prot:string): Promise<any> {
+export default async function validator(prot:string): Promise<any> {
   let proteins = await mapToUniPort(prot);
-  console.log(proteins)
   assert.notEqual(proteins.length, 0); 
   //if no entries found then input does not correspond to a protein in UniProt
   return prot
 }
+
+validator("OCT4").then(res=>{console.log(res)}).catch(err=>{throw err})
   
